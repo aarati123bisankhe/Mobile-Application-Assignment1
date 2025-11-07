a9073b99d6da4658532d6c56625fc33e528a7c4d
aarati123bisankhe <151612973+aarati123bisankhe@users.noreply.github.com>
2025-11-07 10:18:29 +0545

    first commit

---
+++ dart_banking_system/bin/dart_banking_system.dart
@@ -0,0 +1,5 @@
+import 'package:dart_banking_system/dart_banking_system.dart' as dart_banking_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_banking_system.calculate()}!');
+}
